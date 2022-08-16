b0d253dc2550695f6c6b42e23a5f74c7bd827f22
solomon <ssbothwell@gmail.com>
2022-08-16 14:48:47 -0700

    Use an iterator to exit loop

--- maximum/maximum.s
+++ maximum/maximum.s
@@ -6,6 +6,7 @@
 # %edi - Holds the index of the data item being examined
 # %ebx - Largest data item found
 # %eax - Current data item
+# %ecx - An iterator value for my while loop
 #
 # The following memory locations are used:
 #
@@ -14,18 +15,25 @@
 #
 	      .section .data
 data_items:
-	      .long 3,67,34,222,45,75,54,34,44,33,22,11,66,0
+	      .long 3,67,34,222,45,75,54,34,44,33,22,11,66
+
+data_length:
+	      .long 13
 
 	      .section .text
 
 	      .globl _start
 _start:
-	      movl  $0, %edi                   # Move 0 into the index register
-	      movl  data_items(,%edi,4), %eax # Load the first byte of data
+
+	      movl  data_length, %ecx          # Load the list length into ecx
+
+	      movl  $0, %edi                   # Load 0 into the index register
+	      movl  data_items(,%edi,4), %eax  # Load the first byte of data
 	      movl  %eax, %ebx                 # Since this is the first item. %eax holds the largest value
 
 start_loop:
-	      cmpl  $0, %eax                   # Check if we have hit the end of the list
+	      cmpl  $0, %ecx                   # Check if we have hit the end of the list
+	      dec   %ecx                       # Decrement ecx by 1
 	      je    loop_exit
 	      incl  %edi                       # Increment the index
 	      movl  data_items(,%edi,4), %eax  # Load the next byte of data
@@ -35,5 +43,5 @@ start_loop:
 	      jmp   start_loop                 # And jump to start of loop
 
 loop_exit:
-movl $1, %eax                   # Set status code to 1
-int $0x80
+	      movl  $1, %eax                   # Set status code to 1
+	      int   $0x80
